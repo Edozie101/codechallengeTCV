cb27c3d2be6ffec6d302a2a314d4e83b96b4f09f
Edozie Izegbu <edozie@skinanalytics.co.uk>
2020-08-15 12:53:31 +0100

    added server functionality

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+const app = express()
+require('./database')
+
+// requirements
+
+// initialization 
+
+app.use(bodyParser.json())
+app.use(cors())
+
+const users = require('api/users')
+const notes = require('api/notes')
+const appointments = require('api/appointments')
+
+//will add other api functions soon
+app.use('/api/users', users);
+app.use(express.static(path.join(__dirname, '../build')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+})
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+});
+
+

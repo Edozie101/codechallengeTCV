67a0cead6b4723cf85fa3189a60714aa79d7db6e
Edozie Izegbu <edozie@skinanalytics.co.uk>
2020-08-15 15:26:21 +0100

    added Table and entry

--- local/src/App.js
+++ local/src/App.js
@@ -1,19 +1,19 @@
-import React, {useState,useEffect} from 'react';
-import { Button, Form } from 'semantic-ui-react'
+import React, { useState, useEffect } from 'react';
+import { Button, Header,Icon, Form, Table } from 'semantic-ui-react'
 import logo from './logo.svg';
 import axios from 'axios'
 import 'semantic-ui-css/semantic.min.css'
 import './App.css';
 
 function App() {
-  const [users,setUsers] = useState(null)
+  const [users, setUsers] = useState(null)
   const [username, setUserName] = useState("")
   const [email, setEmail] = useState("")
   const [phone, setPhone] = useState("")
 
-  const phoneNumberEntry = (e) => { 
-      setPhone(e.target.value)
-  } 
+  const phoneNumberEntry = (e) => {
+    setPhone(e.target.value)
+  }
   const emailEntry = (e) => {
     setEmail(e.target.value)
   }
@@ -22,28 +22,70 @@ function App() {
     setUserName(e.target.value)
   }
   useEffect(() => {
-		axios
-			.get("/api/users")
-			.then((users) => setUsers(users))
-			.catch((err) => console.log(err));
+    axios
+      .get("/api/users")
+      .then((users) => setUsers(users))
+      .catch((err) => console.log(err));
   }, []);
-  
+
   function submitForm() {
-      if(username || email || phone === "") {
-        alert("Fill all required fields")
-      }
-      axios
-        .post("/api/users",{
-          username: username,
-          email: email,
-          phone: phone
-        })
+    // if (username || email || phone === "") {
+    //   alert("Fill all required fields")
+    // }
+    axios
+      .post("/api/users", {
+        username: username,
+        email: email,
+        phone: phone
+      })
+      .then(function () {
+        alert("User successfully added");
+        window.location.reload()
+      })
+      .catch(function (e) {
+          console.log(e)
+      })
   }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Form>
+        <Header as='h2' icon>
+          <Icon name='settings' />
+            Admin
+    <Header.Subheader>
+            Manage your current users
+    </Header.Subheader>
+        </Header>
+
+        { 
+          (users === null) ? (<p> Loading </p>)
+          : (users.length === 0) ? (<p> No users available</p>) :
+          (<React.Fragment>
+            <Table >
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>Name</Table.HeaderCell>
+                  <Table.HeaderCell>Email</Table.HeaderCell>
+                  <Table.HeaderCell>Phone</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                { users.length > 0 ? users.map(user => {
+                  return(<Table.Row>
+                     <Table.Cell> {user.username} </Table.Cell>
+                     <Table.Cell> {user.email} </Table.Cell>
+                     <Table.Cell> {user.phone} </Table.Cell>
+
+
+                  </Table.Row>)
+                }) : <></> }
+              </Table.Body>
+              </Table>
+          </React.Fragment>)
+
+
+        }
+        <Form onSubmit={submitForm()}>
           <Form.Field>
             <Form.Input label='Enter Name' type='text' />
 
@@ -57,11 +99,11 @@ function App() {
 
           </Form.Field>
 
-          <Button > Submit </Button>
+          <Form.Button> Submit </Form.Button>
 
         </Form>
 
-       
+
       </header>
     </div >
   );
